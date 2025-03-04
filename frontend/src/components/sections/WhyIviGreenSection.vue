3d4ffb575954a0149c5eb3e6ec302ca66f0f3b57
agafonov_sv <sv.agafonov@stc.team>
2025-03-04 09:40:54 +0300

    step_2_back_and_front

--- frontend/src/components/sections/WhyIviGreenSection.vue
+++ frontend/src/components/sections/WhyIviGreenSection.vue
@@ -57,20 +57,23 @@
           </div>
         </div>
       </div>
-
       <div class="pt-2 text-center">
         <a href="#" class="btn btn-success btn-show-all-kits poppins-bold p-3 fw-bold" role="button" data-bs-toggle="button">Show all kits</a>
       </div>
 
-      <div class="mt-5 pt-5">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <div class="mt-5 pb-5 pt-0 py-md-5 my-md-5">
+        <div id="carouselIviGreen" class="carousel slide" data-bs-ride="true">
+          <div class="carousel-indicators d-flex d-md-none">
+            <button type="button" data-bs-target="#carouselIviGreen" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselIviGreen" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselIviGreen" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselIviGreen" data-bs-slide-to="3" aria-label="Slide 4"></button>
           </div>
-          <div class="carousel-inner" style="max-width: 992px;">
+          <div class="carousel-inner" style="max-width: 992px; min-height: 480px;">
             <div class="carousel-item active">
+              <img src="@/assets/img/image.png" class="d-block w-100 img-fluid" style="min-height: 480px;" alt="">
+            </div>
+            <div class="carousel-item">
               <img src="@/assets/img/image.png" class="d-block w-100" alt="">
             </div>
             <div class="carousel-item">
@@ -80,14 +83,64 @@
               <img src="@/assets/img/image.png" class="d-block w-100" alt="">
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <button class="carousel-control-prev d-none d-md-flex" type="button" data-bs-target="#carouselIviGreen" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon " aria-hidden="true"></span>
             <span class="visually-hidden">Предыдущий</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button class="carousel-control-next d-none d-md-flex" type="button" data-bs-target="#carouselIviGreen" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Следующий</span>
           </button>
+        </div>
+      </div>
+
+      <div class="pt-2 pt-md-0 py-md-5 my-md-5">
+        <div>
+          <h2 class="poppins-bold h2-text text-uppercase tc-2C2C2C text-center">
+            The most in-demand topics
+          </h2>
+        </div>
+        <div class="row justify-content-center g-3 mt-2 pt-2 ">
+          <div class="col col-md-4">
+            <a href="#" class="topic-block d-flex flex-row align-items-center text-decoration-none">
+              <div class="topic-block-icon relationships-intelligence-icon w-100"></div>
+              <div class="topic-block-text flex-grow-1 px-3 px-md-4">
+                <h3 class="poppins-medium h3-text-24px tc-3E468F my-0 text-nowrap">Relationships</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a href="#" class="topic-block d-flex flex-row align-items-center text-decoration-none">
+              <div class="topic-block-icon skills-development-icon w-100"></div>
+              <div class="topic-block-text flex-grow-1 px-3 px-md-4">
+                <h3 class="poppins-medium h3-text-24px tc-3E468F my-0 text-nowrap">Skills development</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col col-md-4">
+            <a href="#" class="topic-block d-flex flex-row align-items-center text-decoration-none">
+              <div class="topic-block-icon growth-mindset-icon w-100"></div>
+              <div class="topic-block-text flex-grow-1 px-3 px-md-4">
+                <h3 class="poppins-medium h3-text-24px tc-3E468F my-0 text-nowrap">Growth mindset</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col col-md-5">
+            <a href="#" class="topic-block d-flex flex-row align-items-center text-decoration-none">
+              <div class="topic-block-icon emotional-intelligence-icon w-100"></div>
+              <div class="topic-block-text flex-grow-1 px-3 px-md-4 flex-fill">
+                <h3 class="poppins-medium h3-text-24px tc-3E468F my-0 text-nowrap">Emotional intelligence</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col col-md-5">
+            <a href="#" class="topic-block d-flex flex-row align-items-center g-1 text-decoration-none">
+              <div class="topic-block-icon self-esteem-and-confidence-icon w-100"></div>
+              <div class="topic-block-text px-3 px-md-4">
+                <h3 class="poppins-medium h3-text-24px tc-3E468F my-0 text-wrap">Self-esteem and confidence</h3>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
